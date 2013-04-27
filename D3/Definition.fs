7936511745e02f83c417d97daaa28c9d822ba78b
David Grenier <davidgrenier@gmail.com>
2013-04-27 19:33:04 -0400

    More extension members

--- D3/Definition.fs
+++ D3/Definition.fs
@@ -1,10 +1,56 @@
 ﻿module D3.Definition
 
 open IntelliFactory.WebSharper.InterfaceGenerator
+type E = IntelliFactory.WebSharper.Dom.Element
 
 let d3js = Resource "D3" "http://d3js.org/d3.v3.min.js"
 
 let (=>) name o = name => o |> WithSourceName name
+let (=?) name o = name =? o |> WithSourceName name
+
+let comparator =
+    Class "d3.comparator"
+    |> WithSourceName "d3.comparator"
+
+let chord =
+    let chord = Class "d3.layout.chord"
+    chord
+    |+> [
+        Constructor T<unit>
+    ]
+    |+> Protocol [
+        "padding" => T<float> ^-> chord
+        "sortSubgroups" => comparator ^-> chord
+        "matrix" => T<int [] [] -> unit>
+    ]
+    |> WithSourceName "chord"
+
+let layout =
+    Class "d3.layout"
+    |=> Nested [
+            chord
+        ]
+    |> WithSourceName "layout"
+
+let ordinalScale =
+    let ordinal = Type.New()
+    Class "d3.scale.ordinal"
+    |=> ordinal
+    |+> [
+        Constructor T<unit>
+    ]
+    |+> Protocol [
+        "domain" => T<int []> ^-> ordinal
+        "range" => T<int [] -> int -> unit>
+    ]
+    |> WithSourceName "ordinal"
+
+let scale =
+    Class "d3.scale"
+    |=> Nested [
+            ordinalScale
+        ]
+    |> WithSourceName "scale"
 
 let d3 =
     let d3 = Type.New()
@@ -12,16 +58,26 @@ let d3 =
     |=> d3
     |+> [
         "select" => T<string> ^-> d3
+        "select" => T<E> ^-> d3
         "selectAll" => T<string> ^-> d3
+        "descending" =? comparator
+        "range" => (!? T<int>) * T<int> * (!? T<int>) ^-> T<int []>
     ]
     |+> Protocol [
         "data" => T<int []> ^-> d3
         "style" => T<string> * T<string> ^-> T<unit>
-        "style" => T<string> * (T<unit> ^-> T<string>) ^-> T<unit>
-        "style" => T<string> * (T<int> ^-> T<string>) ^-> T<unit>
-        "style" => T<string> * (T<int> * T<int> ^-> T<string>) ^-> T<unit>
+        "style" => T<string> * T<unit -> string> ^-> T<unit>
+        "style" => T<string> * T<int -> string> ^-> T<unit>
+        "style" => T<string> * T<int * int -> string> ^-> T<unit>
         "transition" => T<unit> ^-> d3
+        "append" => T<string> ^-> d3
+        Generic - fun t ->
+            "attr" => T<string> * t ^-> d3
     ]
+    |=> Nested [
+            layout
+            scale
+        ]
     |> WithSourceName "d3"
     |> Requires [d3js]
 
--- Website/D3.fs
+++ Website/D3.fs
@@ -5,9 +5,12 @@ open IntelliFactory.WebSharper
 open IntelliFactory.WebSharper.Html
 type d3 = Wrappers.D3.d3
 
-let paintBodyBlack() = d3.select("body").style("background-color", "black")
+let black (e: Dom.Element) =
+    d3.select(e).style("background-color", "black")
 
-let paintPs() = d3.selectAll("p").style("color", fun () -> "hsl(" + (random() * 360.0 |> int |> string) + ",100%,50%)")
+let paintPs() =
+    d3.selectAll("p")
+        .style("color", fun () -> "hsl(" + (random() * 360.0 |> int |> string) + ",100%,50%)")
 
 let alternateGray() =
     d3.selectAll("p")
@@ -22,23 +25,61 @@ let setTextSize() =
         .data([|4; 8; 15; 16; 23|])
         .style("font-size", fun (d: int) -> string d + "px")
 
-let backgroundToGrey() =
-    d3.select("body")
+let backgroundToGrey (e: Dom.Element) =
+    d3.select(e)
         .transition()
         .style("background-color", "grey")
 
+let matrix =
+    [|
+        [|11975;  5871; 8916; 2868|]
+        [| 1951; 10048; 2060; 6171|]
+        [| 8010; 16145; 8090; 8045|]
+        [| 1013;   990;  940; 6907|]
+    |]
+
+let chord =
+    d3.layout
+        .chord()
+        .padding(0.05)
+        .sortSubgroups(d3.descending)
+        .matrix(matrix)
+
+let width = 960.0
+let height = 500.0
+let innerRadius = min width height * 0.41
+let outerRadius = innerRadius * 1.1
+
+let fill =
+    d3.scale
+        .ordinal()
+        .domain(d3.range 4)
+        .range([|0; 0xffdd89; 0x957244; 0xf26223|])
+
+let svg =
+    d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + (string (width / 2.0)) + "," + (string (height / 2.0)) + ")")    
+
 let body() =
-    paintBodyBlack()
     Div [
-        P [Text "What I'm asking you to entertain is that there is nothing we need to believe on insufficient evidence in order to have deeply ethical and spiritual lives."]
-        P [Text "I know of no society in human history that ever suffered because its people became too desirous of their core beliefs."]
-        P [Text "Faith enables many of us to endure life's difficulties with an equanimity that would be scarcely conceivable in a world lit only by reason."]
-        P [Text "What can be asserted without proof can be dismissed without proof."]
-        P [Text "Take the risk of thinking for yourself, much more happiness, truth, beauty, and wisdom will come to you that way."]
-        Span []
-    ] |>! OnAfterRender(fun _ ->
-            paintPs()
-            alternateGray()
-            setTextSize()
-            backgroundToGrey()
-        )
+        Div [
+            P [Text "What I'm asking you to entertain is that there is nothing we need to believe on insufficient evidence in order to have deeply ethical and spiritual lives."]
+            P [Text "I know of no society in human history that ever suffered because its people became too desirous of their core beliefs."]
+            P [Text "Faith enables many of us to endure life's difficulties with an equanimity that would be scarcely conceivable in a world lit only by reason."]
+            P [Text "What can be asserted without proof can be dismissed without proof."]
+            P [Text "Take the risk of thinking for yourself, much more happiness, truth, beauty, and wisdom will come to you that way."]
+            Span []
+        ] |>! (fun e -> backgroundToGrey e.Dom)
+        Div [
+            Text "test"
+        ]
+    ] |>! (fun e -> black e.Dom)
+    |>! OnAfterRender(fun _ ->
+        paintPs()
+        alternateGray()
+        setTextSize()
+    )
