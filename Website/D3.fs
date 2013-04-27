f859d76a961567d45d8fcb28ee4debe5b356ea60
David Grenier <davidgrenier@gmail.com>
2013-04-27 14:55:54 -0400

    Initial commit

---
+++ Website/D3.fs
@@ -0,0 +1,10 @@
+﻿namespace Website
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+
+type D3Examples() =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = Div [] :> _
---
+++ Website/Site.fs
@@ -0,0 +1,25 @@
+﻿namespace Website
+
+open IntelliFactory.Html
+open IntelliFactory.WebSharper.Sitelets
+
+type Pages =
+    | [<CompiledName "/">] Home
+    | D3
+
+type Site() =
+    interface IWebsite<Pages> with
+        member __.Actions = []
+        member __.Sitelet =
+            Sitelet.Infer <| function
+                | Home -> Content.Redirect D3
+                | D3 ->
+                    PageContent <| fun ctx ->
+                        {
+                            Page.Default with
+                                Title = Some "D3 Examples"
+                                Body = [ Div [ new D3Examples() ] ]
+                        }
+
+[<assembly: Website(typeof<Site>)>]
+do ()
